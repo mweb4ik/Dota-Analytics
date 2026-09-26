5ac0ec9a0e00d2ef65889789c2602bc62e3ee211
mweb4ik <kiseluk780@gmail.com>
2026-09-26 18:20:30 +0300

    refactor(backend): remove players include from matches list, clean up WPF players grid to show only available API stats

--- backend/DotaAnalytics.Api/Endpoints/MatchesEndpoint.cs
+++ backend/DotaAnalytics.Api/Endpoints/MatchesEndpoint.cs
@@ -60,7 +60,7 @@ public static class MatchesEndpoints
         int skip = (page - 1) * pageSize;
 
         var matches = await query
-            .Include(m => m.Players)
+            //.Include(m => m.Players)
             .Skip(skip)
             .Take(pageSize)
             .ToListAsync();
